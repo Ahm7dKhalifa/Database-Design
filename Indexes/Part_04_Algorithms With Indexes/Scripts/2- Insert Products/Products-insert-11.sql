abcc761b0eea54b6567eca7612a7fc0589eecf66
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-09-24 16:13:30 +0200

    UPDATE Indexes\Part_03_Clustered and Non-Clustered Indexes\ Folder

---
+++ Indexes/Part_04_Algorithms With Indexes/Scripts/2- Insert Products/Products-insert-11.sql
@@ -0,0 +1,74 @@
+
+INSERT INTO [dbo].[Product]
+           ([ID]
+           ,[Name]
+           ,[Price]
+           ,[AvailableQuantity])
+     VALUES
+           (1000001
+           ,'product 100001'
+           ,10001
+           ,101)
+GO
+
+INSERT INTO [dbo].[Product]
+           ([ID]
+           ,[Name]
+           ,[Price]
+           ,[AvailableQuantity])
+     VALUES
+           (1000002
+           ,'product 100002'
+           ,10001
+           ,101)
+GO
+
+INSERT INTO [dbo].[Product]
+           ([ID]
+           ,[Name]
+           ,[Price]
+           ,[AvailableQuantity])
+     VALUES
+           (1000003
+           ,'product 100003'
+           ,10001
+           ,101)
+GO
+
+INSERT INTO [dbo].[Product]
+           ([ID]
+           ,[Name]
+           ,[Price]
+           ,[AvailableQuantity])
+     VALUES
+           (1000001
+           ,'product 100004'
+           ,10001
+           ,101)
+GO
+
+INSERT INTO [dbo].[Product]
+           ([ID]
+           ,[Name]
+           ,[Price]
+           ,[AvailableQuantity])
+     VALUES
+           (1000001
+           ,'product 100004'
+           ,10001
+           ,101)
+GO
+
+INSERT INTO [dbo].[Product]
+           ([ID]
+           ,[Name]
+           ,[Price]
+           ,[AvailableQuantity])
+     VALUES
+           (1000005
+           ,'product 100005'
+           ,10001
+           ,101)
+GO
+
+
